508ba492dc74f9099c23d1cf8e721984b5dc2564
Dennis Heibült <dennis@csshugs.com>
2017-02-10 09:17:21 +0100

    Adjust fractal config

--- fractal.js
+++ fractal.js
@@ -6,8 +6,17 @@ const fractal = module.exports = require('@frctl/fractal').create();
 /* Set the title of the project */
 fractal.set('project.title', 'inuitcss-fractal-scaffold');
 
+/* Destination for the static export */
+fractal.web.set('builder.dest', 'build');
+
 /* Tell Fractal where the components will live */
 fractal.components.set('path', __dirname + '/src/components');
 
 /* Tell Fractal where the documentation pages will live */
 fractal.docs.set('path', __dirname + '/src/docs');
+
+/* Tell Fractal what default preview template to use */
+fractal.components.set('default.preview', '@preview');
+
+/* Specify a directory of static assets (fonts/css etc.) */
+fractal.web.set('static.path', __dirname + '/public');
