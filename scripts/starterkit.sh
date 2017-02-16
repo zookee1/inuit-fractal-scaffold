bda0f8c10076cc4c69a0781bffbc15e5263cf312
zookee1 <hi@zookee.one>
2017-02-16 23:38:57 +0100

    added command npm run starterkit which inserts atomic-design-fractal-scaffold into src

---
+++ scripts/starterkit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PROJECTDIR="$PWD"
+SRCDIR="$PROJECTDIR/src/"
+STARTERKIT="https://github.com/zookee1/atomic-design-fractal-scaffold"
+
+if [ -d "$SRCDIR" ]; then
+    printf '%s\n' "Removing src ($SRCDIR)"
+    sudo rm -rf "$SRCDIR"
+fi
+
+git clone $STARTERKIT src
